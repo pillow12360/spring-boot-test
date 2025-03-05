c3ebd08ff129d775017e0b9e71c5edce37c1b322
pillow12360 <ehdcks1256@naver.com>
2025-03-05 16:19:59 +0900

    setting: jdbc, notnull 테스트

--- src/main/java/com/uplus/eureka/config/DataBaseConfiguration.java
+++ src/main/java/com/uplus/eureka/config/DataBaseConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.PropertySource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-@Configuration
 @PropertySource("classpath:/application.properties")
 public class DataBaseConfiguration {
 
--- src/test/java/com/uplus/eureka/SpringBootTestApplicationTests.java
+++ src/test/java/com/uplus/eureka/SpringBootTestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.uplus.eureka;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(
+        properties = {"spring.config.location=classpath:application.properties"
+        }
+)
+@ComponentScan(basePackages = {"com.uplus.eureka"})
 class SpringBootTestApplicationTests {
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DataSource ds;
 
     @Test
-    void contextLoads() {
+    public void dsTest() {
+        //null 체크 함수
+        assertNotNull(ds);
     }
 
 }
